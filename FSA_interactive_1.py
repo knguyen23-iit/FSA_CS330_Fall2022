371b809da30f41c70ca25ad58238455ca7475610
knguyen23-iit <116779856+knguyen23-iit@users.noreply.github.com>
2022-11-15 16:59:38 -0600

    Add files via upload

---
+++ FSA_interactive_1.py
@@ -0,0 +1,37 @@
+from FSA_demo_0 import Lock
+import random
+
+
+sample = Lock()
+
+
+def run_count():
+    count = 0
+    i = 0
+    flag=0
+    while flag==0:
+        i = input("Enter your value ('s' to stop): ")
+        if i=='s':
+            flag=1
+            break
+        state_before = sample.state
+        sample.entry(i)
+        count+=1
+        state_after = sample.state
+        if state_before==0 and state_after==1:
+            print('\n')
+            print('Hurray! It took you',count,'entries to unlock!')
+            print('The lock pad is set back to all 0')
+            print('You can try to lock it if you want to?')
+            count = 0 
+        if state_before==1 and state_after==0:
+            print('It took you',count,'times to lock it!')
+            print('The lock pad is set back to all 0')
+            print('You can try to unlock it if you want to?')
+            count = 0 
+        print(sample.lock[0],sample.lock[1],
+              sample.lock[2],sample.lock[3],sample.lock[4],sample.lock[5])
+    return count
+
+run_count()
+
