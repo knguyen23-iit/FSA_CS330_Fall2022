4d49ec30b5ad0e34fdd4a433c3ef793591f5c399
knguyen23-iit <knguyen23@hawk.iit.edu>
2022-11-17 01:29:09 -0600

    Update

--- FSA_interactive_1.py
+++ FSA_interactive_1.py
@@ -9,10 +9,17 @@ def run_count():
     count = 0
     i = 0
     flag=0
+    print('\n')
+    print('1 is unlocked and 0 is locked.')
+    print('Progress of lock pad:')
+    print(sample.lock[0],sample.lock[1],
+          sample.lock[2],sample.lock[3],sample.lock[4],sample.lock[5])
     while flag==0:
+        print('Unlock by 5-2-5-2-7-1. Lock by 5-2-5-2-7-4.')
         i = input("Enter your value ('s' to stop): ")
         if i=='s':
             flag=1
+            print('Thank you for your time!')
             break
         state_before = sample.state
         sample.entry(i)
@@ -29,7 +36,10 @@ def run_count():
             print('It took you',count,'entries to lock it!')
             print('The lock pad is set back to all 0')
             print('You can try to unlock it if you want to?')
-            count = 0 
+            count = 0
+        print('\n')
+        print('1 is unlocked and 0 is locked.')
+        print('Progress of lock pad:')
         print(sample.lock[0],sample.lock[1],
               sample.lock[2],sample.lock[3],sample.lock[4],sample.lock[5])
     return count
